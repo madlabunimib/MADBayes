5ccbcf4cbf79282b895e752f1a780b1077dad5da
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-02-17 19:00:09 +0100

    Add from string to graph parser

---
+++ weighted_imputation/utils/parser.py
@@ -0,0 +1,27 @@
+import re
+import numpy as np
+from ..structure import Node, Graph
+
+
+def parse_graph_from_string(string: str) -> Graph:
+    pattern = re.compile(r"\[(\w*)(?:\|(\w+[:\w+]*)){0,1}\]")
+    edges = re.findall(pattern, string)
+    edges = [
+        (parent, child)
+        for (child, parents) in edges
+        for parent in parents.split(':')
+        if len(parent) > 0
+    ]
+    nodes = set()
+    for parent, child in edges:
+        nodes.add(parent)
+        nodes.add(child)
+    nodes = list(nodes)
+    n = len(nodes)
+    adjacency_matrix = np.zeros((n, n), dtype=bool)
+    for parent, child in edges:
+        i = nodes.index(parent)
+        j = nodes.index(child)
+        adjacency_matrix[i, j] = True
+    nodes = [Node(label) for label in nodes]
+    return Graph(nodes, adjacency_matrix)
