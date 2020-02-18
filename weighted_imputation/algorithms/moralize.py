6c119fe30ce6a92aabc524afff10649a9bbe9f3f
AlessioZanga <alessio.zanga@outlook.it>
2020-02-18 13:34:12 +0100

    Add autoparallel switch for graphs with more than 50 nodes

--- weighted_imputation/algorithms/moralize.py
+++ weighted_imputation/algorithms/moralize.py
@@ -1,19 +1,34 @@
 import numpy as np
 from numba import njit, prange
-from itertools import combinations
 from ..structure import Graph
 
 
-def moralize(graph: Graph) -> Graph:
+def moralize(graph: Graph, force_parallel: bool = False) -> Graph:
     A = graph.get_adjacency_matrix()
     out = np.zeros(A.shape, dtype=bool)
-    _moralize(A, out)
-    graph.set_adjacency_matrix(out)
-    return graph
+    if not force_parallel and A.shape[0] < 50:
+        _moralize(A, out)
+    else:
+        _moralize_parallel(A, out)
+    return Graph(graph.get_nodes(), out)
+
+
+@njit
+def _moralize(A, out):
+    n = A.shape[0]
+    for columns in range(n):
+        parents = A.T[columns]
+        indexes = np.nonzero(parents)[0].T
+        for i in indexes:
+            for j in indexes:
+                if i != j:
+                    out[i, j] = True
+    np.bitwise_or(A, out, out)
+    np.bitwise_or(out, out.T, out)
 
 
 @njit(parallel=True)
-def _moralize(A, out):
+def _moralize_parallel(A, out):
     n = A.shape[0]
     for columns in prange(n):
         parents = A.T[columns]
