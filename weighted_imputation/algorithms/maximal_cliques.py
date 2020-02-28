2ad02b567cbcd6b7ae45f6a2e3cbc2731b61c162
AlessioZanga <alessio.zanga@outlook.it>
2020-02-28 13:59:52 +0100

    Update maximal_cliques

--- weighted_imputation/algorithms/maximal_cliques.py
+++ weighted_imputation/algorithms/maximal_cliques.py
@@ -2,6 +2,7 @@ import numpy as np
 from numba import njit
 from typing import List, Set
 from ..structure import Graph
+from ..utils import union, intersection, difference
 
 
 def maximal_cliques(graph: Graph) -> List:
@@ -20,33 +21,33 @@ def maximal_cliques(graph: Graph) -> List:
 # Initialize jitted version of _bron_kerbosh by using empty typed sets
 @njit(cache=True)
 def _maximal_cliques(adjacency_matrix: np.ndarray, nodes_indexes: List[int]) -> List:
-    A = set([0 for _ in range(0)])
-    B = set(nodes_indexes)
-    C = set([0 for _ in range(0)])
+    A = np.array([0 for _ in range(0)])
+    B = np.array(nodes_indexes)
+    C = np.array([0 for _ in range(0)])
     return _bron_kerbosh(adjacency_matrix, A, B, C)
 
 @njit
-def _neighbor_set(adjacency_matrix: np.ndarray, node: int) -> Set[int]:
+def _neighbor_set(adjacency_matrix: np.ndarray, node: int) -> np.ndarray:
     A = adjacency_matrix[node]
-    neighbor = np.nonzero(A)[0].T
-    neighbor_set = set(neighbor)
+    neighbor_set = np.nonzero(A)[0].T
     return neighbor_set
 
-@njit
-def _bron_kerbosh(adjacency_matrix: np.ndarray, A: Set[int], B: Set[int], C: Set[int]) -> List:
+@njit(parallel=True)
+def _bron_kerbosh(adjacency_matrix: np.ndarray, A: np.ndarray, B: np.ndarray, C: np.ndarray) -> List:
     if len(B) == 0 and len(C) == 0:
         return [A]
-    # TODO: Change reflected list to  typed list
-    emptyset = set([0 for _ in range(0)])
-    result = [emptyset for _ in range(0)]
-    for node in B.copy():
-        neighbor_set = _neighbor_set(adjacency_matrix, node)
+    result = [np.array([0 for _ in range(0)]) for _ in range(0)]
+    X = B.copy()
+    n = len(X)
+    for i in range(n):
+        node = np.array([X[i]])
+        neighbor_set = _neighbor_set(adjacency_matrix, X[i])
         result += _bron_kerbosh(
             adjacency_matrix,
-            A.union({node}),
-            B.intersection(neighbor_set),
-            C.intersection(neighbor_set)
+            union(A, node),
+            intersection(B, neighbor_set),
+            intersection(C, neighbor_set)
         )
-        B = B.difference({node})
-        C = C.union({node})
+        B = difference(B, node)
+        C = union(C, node)
     return result
