74bec60dbdede0e332b1f288afb9fdca754be5e5
AlessioZanga <alessio.zanga@outlook.it>
2020-02-28 13:14:45 +0100

    Update set operation implementation

---
+++ tests/test_set_operations.py
@@ -0,0 +1,24 @@
+import numpy as np
+from . import weighted_imputation as wi
+
+def test_set():
+    a = [0, 1, 2, 3]
+    b = [2, 3, 4, 5]
+    # Test union
+    x = set(a).union(set(b))
+    x = sorted(list(x))
+    y = wi.union(np.array(a), np.array(b))
+    y = sorted(list(y))
+    assert(x == y)
+    # Test intersection
+    x = set(a).intersection(set(b))
+    x = sorted(list(x))
+    y = wi.intersection(np.array(a), np.array(b))
+    y = sorted(list(y))
+    assert(x == y)
+    # Test difference
+    x = set(a).difference(set(b))
+    x = sorted(list(x))
+    y = wi.difference(np.array(a), np.array(b))
+    y = sorted(list(y))
+    assert(x == y)
--- weighted_imputation/utils/set.py
+++ weighted_imputation/utils/set.py
@@ -1,6 +1,8 @@
 import numpy as np
+from numba import njit
 
 
+@njit
 def union(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     if len(a.shape) != 1 or len(b.shape) != 1:
         raise Exception('Arrays must be 1D.')
@@ -8,16 +10,22 @@ def union(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     out = np.unique(out)
     return out
 
+@njit
 def intersection(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     if len(a.shape) != 1 or len(b.shape) != 1:
         raise Exception('Arrays must be 1D.')
     out = np.concatenate((a, b))
     out = np.bincount(out)
-    out = np.argwhere(out > 1)
-    out = out.T[0]
+    out = np.nonzero(out > 1)[0]
+    out = out.T
     return out
 
+@njit
 def difference(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     if len(a.shape) != 1 or len(b.shape) != 1:
         raise Exception('Arrays must be 1D.')
-    pass
+    out = np.concatenate((a, b, b))
+    out = np.bincount(out)
+    out = np.nonzero(out == 1)[0]
+    out = out.T
+    return out
