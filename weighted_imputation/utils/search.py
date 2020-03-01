28d6b02c0ec6856c4559f763749c438ab662d423
AlessioZanga <alessio.zanga@outlook.it>
2020-03-01 01:17:23 +0100

    Update function signatures

--- weighted_imputation/utils/search.py
+++ weighted_imputation/utils/search.py
@@ -6,37 +6,37 @@ from ..structures import Graph
 
 
 def DFS(graph: Graph) -> np.ndarray:
-    adjacency_matrix = graph.get_adjacency_matrix()
-    n = adjacency_matrix.shape[0]
+    A = graph.get_adjacency_matrix()
+    n = A.shape[0]
     color = np.ones(n, dtype=bool)
     times = -np.ones((n, 2), dtype=int)
     parents = -np.ones(n, dtype=int)
-    _DFS(n, adjacency_matrix, color, times, parents)
+    _DFS(A, color, times, parents)
     return {'parents': parents, 'times': times}
 
 @njit(cache=True)
 def _DFS(
-        n: int,
         A: np.ndarray,
         color: np.ndarray,
         times: np.ndarray,
         parents: np.ndarray
     ) -> None:
     time = 0
+    n = A.shape[0]
     for i in range(n):
         if color[i]:
-            time = _DFS_Visit(i, time, n, A, color, times, parents)
+            time = _DFS_Visit(i, time, A, color, times, parents)
 
 @njit
 def _DFS_Visit(
         node: int,
         time: int,
-        n: int,
         A: np.ndarray,
         color: np.ndarray,
         times: np.ndarray,
         parents: np.ndarray
     ) -> int:
+    n = A.shape[0]
     color[node] = False
     times[node, 0] = time
     time = time + 1
@@ -44,7 +44,7 @@ def _DFS_Visit(
         if A[node][i]:
             if color[i]:
                 parents[i] = node
-                time = _DFS_Visit(i, time, n, A, color, times, parents)
+                time = _DFS_Visit(i, time, A, color, times, parents)
     times[node, 1] = time
     return time + 1
 
