52fbe013bfe9fa1f910de36a47eff5f6d3391086
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-03-02 16:37:33 +0100

    Fix imports

--- weighted_imputation/algorithms/__init__.py
+++ weighted_imputation/algorithms/__init__.py
@@ -1,4 +1,4 @@
 from .moralize import moralize
 from .triangulate import triangulate
-from .min_qs import min_qs_triangulate
+from .min_qs import min_qs_triangulate, bfs
 from .maximal_cliques import maximal_cliques
