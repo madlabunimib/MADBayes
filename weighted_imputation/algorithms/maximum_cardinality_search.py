df78337e34bf17d04537a7e3b9d0e25af0947f12
AlessioZanga <alessio.zanga@outlook.it>
2020-03-04 01:45:23 +0100

    Update MCS Fill-In

--- weighted_imputation/algorithms/maximum_cardinality_search.py
+++ weighted_imputation/algorithms/maximum_cardinality_search.py
@@ -25,48 +25,44 @@ def _maximum_cardinality_search(node: int, A: np.ndarray) -> np.ndarray:
     for i in range(1, n):
         X = difference(X, numbering)
         x = X.shape[0]
-        max_val = -1
-        max_pos = -1
+        vmax = -1
+        pmax = -1
         for j in range(x):
             k = len(intersection(neighbors[X[j]], numbering))
-            if max_val < k:
-                max_val = k
-                max_pos = j
-        numbering = np.append(numbering, [X[max_pos]])
+            if vmax < k:
+                vmax = k
+                pmax = j
+        numbering = np.append(numbering, [X[pmax]])
     return numbering
 
 def maximum_cardinality_search_fill_in(graph: Graph, return_new_edges: bool = False) -> np.ndarray:
     adjacency_matrix = graph.get_adjacency_matrix()
     new_edges = np.zeros(adjacency_matrix.shape, dtype=bool)
     _maximum_cardinality_search_fill_in(0, adjacency_matrix, new_edges)
-    traingulated_graph = Graph(graph.get_nodes(), adjacency_matrix)
+    traingulated = Graph(graph.get_nodes(), adjacency_matrix)
     if return_new_edges:
-        return traingulated_graph, new_edges
-    return traingulated_graph
+        return traingulated, new_edges
+    return traingulated
 
-# @njit(cache=True)
+@njit(cache=True)
 def _maximum_cardinality_search_fill_in(node: int, A: np.ndarray, out: np.ndarray) -> np.ndarray:
     n = A.shape[0]
-    neighbors = Dict()
     numbering = np.array([node])
     X = np.array([i for i in range(n)])
-    for i in range(n):
-        # Caching neighbors sets
-        neighbors[X[i]] = _neighbors(X[i], A)
     for i in range(1, n):
         X = difference(X, numbering)
         x = X.shape[0]
-        max_val = -1
-        max_pos = -1
+        vmax = -1
+        pmax = -1
         for j in range(x):
-            k = len(intersection(neighbors[X[j]], numbering))
-            if max_val < k:
-                max_val = k
-                max_pos = j
-        numbering = np.append(numbering, [X[max_pos]])
-        nodes = intersection(neighbors[X[max_pos]], numbering[:i])
-        fill_in_set = _fill_in_set(nodes, A)
-        m = fill_in_set.shape[0]
+            k = len(intersection(_neighbors(X[j], A), numbering))
+            if vmax < k:
+                vmax = k
+                pmax = j
+        numbering = np.append(numbering, [X[pmax]])
+        nodes = intersection(_neighbors(X[pmax], A), numbering[:i])
+        indices = _fill_in_set(nodes, A)
+        m = indices.shape[0]
         for l in range(m):
-            out[fill_in_set[l]] = True
+            out[indices[l, 0], indices[l, 1]] = True
     np.bitwise_or(out, A, A)
