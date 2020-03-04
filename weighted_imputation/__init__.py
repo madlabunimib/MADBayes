456e67a066d7111ad8f42d57b86dfe044f56f0a8
AlessioZanga <alessio.zanga@outlook.it>
2020-03-04 02:06:21 +0100

    Add BFS placeholder

--- weighted_imputation/__init__.py
+++ weighted_imputation/__init__.py
@@ -1,4 +1,4 @@
-from .algorithms import bfs, DFS, moralize, triangulate, maximal_cliques
+from .algorithms import BFS, DFS, moralize, triangulate, maximal_cliques
 from .algorithms import min_qs_triangulate, maximum_cardinality_search, maximum_cardinality_search_fill_in
 from .structures import Graph, DirectedGraph
 from .utils import ST, RMQ, LCA, union, intersection, difference, import_dataset, get_frequencies
---
+++ weighted_imputation/algorithms/breadth_first_search.py
@@ -0,0 +1,6 @@
+from typing import Dict
+from ..structures import Graph
+
+
+def BFS(graph: Graph) -> Dict:
+    pass
