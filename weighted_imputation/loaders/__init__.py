567dbda6adc7ccb26244c8f99afdcc5933bc676e
AlessioZanga <alessio.zanga@outlook.it>
2020-03-06 13:28:35 +0100

    Add dsc parser

--- weighted_imputation/loaders/__init__.py
+++ weighted_imputation/loaders/__init__.py
@@ -1,1 +1,2 @@
 from .bif import parse_bif
+from .dsc import parse_dsc
---
+++ weighted_imputation/loaders/dsc.py
@@ -0,0 +1,78 @@
+from lark import Lark, tree
+
+GRAMMAR = r"""
+
+  start: compilationunit
+  %ignore " "
+  %ignore "\t"
+  %ignore "\n"
+  %ignore "\r"
+  %ignore "//"
+  %ignore "/*"
+  %ignore "\""
+  %ignore ","
+  %ignore "|"
+
+  WORD: LETTER (LETTER | DIGIT)* 
+  LETTER: /[a-zA-Z_-]/ 
+  DIGIT:  /[0-9]/ 
+
+  DECIMAL_LITERAL: /[0-9][1-9]*/ 
+  FLOATING_POINT_LITERAL: /[0-9]+\.[0-9]*/ (EXPONENT)? 
+        | /\.[0-9]/ (EXPONENT)? 
+        | /[0-9]+/ (EXPONENT)? 
+  EXPONENT: ("e"|"E") ("+"|"-")? /[0-9]+/
+
+  NETWORK: "belief network" 
+  VARIABLE: "node" 
+  PROBABILITY: "probability" 
+  PROPERTY: "property" 
+  VARIABLETYPE: "type" 
+  DISCRETE: "discrete" 
+
+  PROPERTYSTRING: PROPERTY /[^;]*/ ";"
+
+  compilationunit: networkdeclaration ( variabledeclaration | probabilitydeclaration )*
+
+  networkdeclaration: NETWORK WORD (networkcontent)?
+
+  networkcontent: "{" ( property  )* "}"
+
+  variabledeclaration: VARIABLE probabilityvariablename variablecontent
+
+  variablecontent: "{" ( property | variablediscrete )* "}"
+
+  variablediscrete: VARIABLETYPE ":" DISCRETE "[" DECIMAL_LITERAL "]" "=" "{" variablevalueslist "}" ";"
+
+  variablevalueslist: probabilityvariablevalue ( probabilityvariablevalue )*
+
+  probabilityvariablevalue: (WORD | DECIMAL_LITERAL)
+
+  probabilitydeclaration: PROBABILITY probabilityvariableslist probabilitycontent
+
+  probabilityvariableslist: "(" probabilityvariablename ( probabilityvariablename )* ")"
+
+  probabilityvariablename: (WORD | DECIMAL_LITERAL)
+  
+  probabilitycontent: "{" ( property | probabilityentry | probabilitytable )* "}"
+
+  probabilityentry: probabilityvalueslist ":" floatingpointlist ";"
+
+  probabilityvalueslist: "(" probabilityvariablevalue ( probabilityvariablevalue )* ")"
+
+  probabilitytable: floatingpointlist ";"
+
+  floatingpointlist: FLOATING_POINT_LITERAL ( FLOATING_POINT_LITERAL )*
+
+  property: (WORD | DECIMAL_LITERAL)
+
+"""
+
+def parse_dsc(path: str, debug=False):
+  with open(path, 'r') as file:
+    text = file.read()
+  parser = Lark(GRAMMAR, parser='lalr', debug=True)
+  parsed = parser.parse(text)
+  if debug:
+    tree.pydot__tree_to_png(parsed, './debug.png')
+  return parsed
