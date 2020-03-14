1aef327be11d01e139fe6e33f40002ea637af283
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-03-15 00:08:53 +0100

    Add comput_margin_table function

--- weighted_imputation/__init__.py
+++ weighted_imputation/__init__.py
@@ -1,6 +1,6 @@
 from .algorithms import BFS, DFS, moralize, triangulate, maximal_cliques
 from .algorithms import MCS, chain_of_cliques
 from .structures import BayesianNetwork, Graph, DirectedGraph, Node, Tree, JunctionTree
-from .utils import ST, RMQ, LCA, get_cpts
+from .utils import ST, RMQ, LCA, get_cpts, compute_margin_table
 
 from . import data
--- weighted_imputation/utils/compute_cpts.py
+++ weighted_imputation/utils/compute_cpts.py
@@ -1,6 +1,7 @@
 import xarray as xa
 import numpy as np
-from typing import Dict
+import itertools
+from typing import Dict, List
 
 
 def get_cpts(parsed_file: Dict) -> Dict:
@@ -9,10 +10,9 @@ def get_cpts(parsed_file: Dict) -> Dict:
         cpt = parsed_file[key]
         
         #Compute dimensions
-        dimensions = []
+        dimensions = [key]
         for dim in cpt["dependencies"]:
             dimensions.append(dim)
-        dimensions.append(key)
         
         #Compute coordinates
         coordinates = []
@@ -20,7 +20,7 @@ def get_cpts(parsed_file: Dict) -> Dict:
             coordinates.append(parsed_file[node]["levels"])
 
         #Compute size of n-d-matrix
-        levels = [] 
+        levels = []
         for dim in dimensions:
             levels.append(len(parsed_file[dim]["levels"]))
 
@@ -29,12 +29,46 @@ def get_cpts(parsed_file: Dict) -> Dict:
             for row in cpt["cpt"]:
                 labels = row[0]
                 for i, value in enumerate(cpt["levels"]):
-                    labels.append(value)   
+                    labels.insert(0, value)
                     data.loc[tuple(labels)] = row[1][i]
-                    labels.pop()
+                    labels.pop(0)
         else:
             for i, value in enumerate(cpt["levels"]):
                 data.loc[value] = cpt["cpt"][0][i]
         
         dictionary.update({key : data})
     return dictionary
+
+def compute_margin_table(node: str, cpts_dict: Dict) -> Dict:
+    node_cpt = cpts_dict[node]
+
+    #Compute coordinates
+    coordinates = []
+    for key in node_cpt.coords[node].values:
+        coordinates.append(key)
+    
+    if not isinstance(node, list):
+        node = list(node)
+    node_dependencies = [x for x in node_cpt.coords if x not in node]
+    dependencies_dict = {}
+    for dependencie in node_dependencies:
+        dependencies_dict.update({dependencie : []})
+        for value in cpts_dict[dependencie].coords[dependencie].values:
+            dependencies_dict[dependencie].append(value)
+
+    for coordinate in coordinates:
+        for combination in list(_my_product(dependencies_dict)):
+            label = [x for x in combination.values()]
+            label.insert(0, coordinate)
+            for item in combination:                
+                node_cpt.loc[tuple(label)] = node_cpt.loc[tuple(label)] * \
+                    cpts_dict[item].loc[combination[item]]
+
+    margin_table = xa.DataArray(np.zeros(shape=len(coordinates)), dims=node, coords=[coordinates])
+    for coordinate in coordinates:
+        margin_table.loc[coordinate] = node_cpt.loc[coordinate].sum()
+
+    return margin_table
+
+def _my_product(input: Dict):
+    return (dict(zip(input.keys(), values)) for values in itertools.product(*input.values()))
