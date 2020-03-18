a3eb8e24aa56cb68846b14ea3d81a117182a4934
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-03-18 01:10:15 +0100

    Remove function compute_margin_table

--- weighted_imputation/__init__.py
+++ weighted_imputation/__init__.py
@@ -1,6 +1,6 @@
 from .algorithms import BFS, DFS, moralize, triangulate, maximal_cliques
 from .algorithms import MCS, chain_of_cliques
 from .structures import BayesianNetwork, Graph, DirectedGraph, Node, Tree, JunctionTree, compute_potentials
-from .utils import ST, RMQ, LCA, get_cpts, compute_margin_table
+from .utils import ST, RMQ, LCA, get_cpts
 
 from . import data
--- weighted_imputation/utils/__init__.py
+++ weighted_imputation/utils/__init__.py
@@ -1,3 +1,3 @@
 from .search import ST, RMQ, LCA
 from .set import union, intersection, difference
-from .compute_cpts import get_cpts, compute_margin_table
+from .compute_cpts import get_cpts
