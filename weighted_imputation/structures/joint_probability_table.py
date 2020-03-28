8aeae4dd1ac257059afed45a233e08fac702eb50
AlessioZanga <alessio.zanga@outlook.it>
2020-03-28 01:00:27 +0100

    Remove unnecessary transpose operation during marginalization

--- weighted_imputation/structures/joint_probability_table.py
+++ weighted_imputation/structures/joint_probability_table.py
@@ -14,13 +14,9 @@ class JointProbabilityTable(ProbabilityTable):
         levels = [list(self.levels(v)) for v in variables]
         pt = ProbabilityTable(dims=variables, coords=levels)
 
-        # TODO: Check if JPT must be transposed or it's optional
-        order = variables + [v for v in self.variables() if v not in variables]
-        jpt = self.transpose(*order)
-
         for location in pt.locations():
             # TODO: Find a way to remove the medium pointer
             pointer = tuple([*location.values()])
-            pt.loc[pointer] = jpt(**location).sum()
+            pt.loc[pointer] = self(**location).sum()
 
         return pt
