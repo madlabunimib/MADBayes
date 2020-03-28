35ab0207b42ef6c53f404362692def54932dfc3b
AlessioZanga <alessio.zanga@outlook.it>
2020-03-28 16:14:03 +0100

    Add data networks loading on module import

--- weighted_imputation/data/__init__.py
+++ weighted_imputation/data/__init__.py
@@ -1,1 +1,5 @@
-from .bayesian_network import asia, survey
+import sys
+from .bayesian_network import load_networks_from_disk
+
+for key, value in load_networks_from_disk().items():
+    setattr(sys.modules[__name__], key, value)
--- weighted_imputation/data/bayesian_network.py
+++ weighted_imputation/data/bayesian_network.py
@@ -1,8 +1,18 @@
-from os.path import abspath, dirname, join
+from os import listdir
+from os.path import abspath, basename, dirname, isfile, join, splitext
+from typing import Dict
+
 from ..structures import BayesianNetwork
 
-DATASETS = abspath(join(dirname(__file__), '../datasets'))
-NETWORKS = abspath(join(dirname(__file__), '../networks'))
 
-asia = BayesianNetwork.from_file(join(NETWORKS, 'asia.bif'))
-survey = BayesianNetwork.from_file(join(NETWORKS, 'survey.bif'))
+def load_networks_from_disk() -> Dict:
+    networks = abspath(join(dirname(__file__), '../networks'))
+    networks = [join(networks, network) for network in listdir(networks)]
+    networks = [network for network in networks if isfile(network) and network.endswith('.bif')]
+
+    networks = {
+        splitext(basename(network))[0] : BayesianNetwork.from_file(network)
+        for network in networks
+    }
+
+    return networks
