a88309faed17cd92d454c13a349bd49b11db02da
AlessioZanga <alessio.zanga@outlook.it>
2020-03-31 01:50:44 +0200

    Change numba implementation of bron-kerbosh to backport

--- weighted_imputation/backends/numba/algorithms/bron_kerbosh.py
+++ weighted_imputation/backends/numba/algorithms/bron_kerbosh.py
@@ -31,43 +31,24 @@ def _bron_kerbosh(adjacency_matrix: np.ndarray) -> List:
     A = np.array([0 for _ in range(0)])
     B = np.array([i for i in range(n)])
     C = np.array([0 for _ in range(0)])
-    # Caching neighbors
-    neighbors = IntegerVectorDict()
-    for i in range(n):
-        neighbors[i] = _neighbors(i, adjacency_matrix)
-    return _bron_kerbosh_recursive(neighbors, A, B, C)
+    return _bron_kerbosh_recursive(adjacency_matrix, A, B, C)
 
 @njit(cache=True)
-def _bron_kerbosh_recursive(neighbors: Dict, A: np.ndarray, B: np.ndarray, C: np.ndarray) -> List:
+def _bron_kerbosh_recursive(adjacency_matrix: np.ndarray, A: np.ndarray, B: np.ndarray, C: np.ndarray) -> List:
     if len(B) == 0 and len(C) == 0:
         return [A]
     out = [np.array([0 for _ in range(0)]) for _ in range(0)]
     X = B.copy()
-    n = B.shape[0]
-    # Select a pivot vertex
-    pivot = 0
-    if n > 0:
-        pivot = _bron_kerbosh_pivot(neighbors, B, C)
+    n = X.shape[0]
     for i in range(n):
         node = np.array([X[i]])
-        if len(np.argwhere(neighbors[X[i]] == pivot)) == 0:
-            out += _bron_kerbosh_recursive(
-                neighbors,
-                union(A, node),
-                intersection(B, neighbors[X[i]]),
-                intersection(C, neighbors[X[i]])
-            )
-            B = difference(B, node)
-            C = union(C, node)
+        neighbors = _neighbors(X[i], adjacency_matrix)
+        out += _bron_kerbosh_recursive(
+            adjacency_matrix,
+            union(A, node),
+            intersection(B, neighbors),
+            intersection(C, neighbors)
+        )
+        B = difference(B, node)
+        C = union(C, node)
     return out
-
-@njit(cache=True)
-def _bron_kerbosh_pivot(neighbors: Dict, B: np.ndarray, C: np.ndarray) -> int:
-    # Select the pivot vertex by Cazals-Karande method
-    X = union(B, C)
-    n = X.shape[0]
-    degrees = np.zeros(n)
-    for i in range(n):
-        degrees[i] = len(intersection(B, neighbors[i]))
-    pivot = np.argmax(degrees)
-    return pivot
