0e31063f1c3054237f14d69c90d44aba78d8de14
AlessioZanga <alessio.zanga@outlook.it>
2020-04-02 00:35:35 +0200

    Revert "Remove test chain of clique numba implementation because of loop"
    This reverts commit 60a1c266f977d86d7f94a13e0024e0df6cee9d07.

---
+++ weighted_imputation/backends/numba/algorithms/chain_of_cliques.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+from numba import njit, prange
+
+from .bron_kerbosh import _bron_kerbosh
+from .nodes import _perfect_numbering
+
+if TYPE_CHECKING:
+    from typing import List
+    from ....structures import Graph
+
+
+def chain_of_cliques(graph: Graph) -> List:
+    nodes = graph.nodes()
+    adjacency_matrix = graph.adjacency_matrix()
+    chain = _chain_of_cliques(adjacency_matrix)
+    chain = [[nodes[node] for node in clique] for clique in chain]
+    return chain
+
+@njit(cache=True, parallel=True)
+def _chain_of_cliques(A: np.ndarray) -> List:
+    # Calculate the perfect numbering
+    # starting from the first node
+    numbering = _perfect_numbering(0, A)
+    # Calculate the list of maximal cliques
+    # using the Bron-Kerbosh algorithm
+    cliques = _bron_kerbosh(A)
+    # Assign to each clique the largest perfect
+    # number of its nodes
+    n = len(cliques)
+    order = np.zeros(n)
+    for i in prange(n):
+        clique = cliques[i]
+        vmax = 0
+        for node in clique:
+            number = np.nonzero(numbering == node)[0][0]
+            if vmax < number:
+                vmax = number
+        order[i] = vmax
+    order = np.argsort(order)
+    chain = []
+    for i in range(n):
+        chain.append(cliques[order[i]])
+    return chain
