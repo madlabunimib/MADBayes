f1554fac6dbc016d4c8e7a82d37fbb4c25c68e94
AlessioZanga <alessio.zanga@outlook.it>
2020-04-02 00:08:25 +0200

    Add perfect_numbering_testing

---
+++ tests_numba/test_perfect_numbering.py
@@ -0,0 +1,30 @@
+import numpy as np
+import networkx as nx
+from . import weighted_imputation as wi
+
+def test_perfect_numbering():
+    # Generate random graphs from nodes count
+    nodes = [3, 10, 25, 50]
+    graphs = [
+        nx.gnp_random_graph(n, 0.5, directed=True)
+        for n in nodes
+    ]
+    # Transform to Wi Graphs
+    graphs = [wi.Graph.from_networkx(G) for G in graphs]
+    graphs = [wi.triangulate(graph) for graph in graphs]
+    numberings = [wi.perfect_numbering(graph) for graph in graphs]
+    numberings = [
+        [
+            set(wi.boundary(graphs[i], numbering[:j])).intersection(set(numbering[:j-1]))
+            for j, _ in enumerate(numbering)
+        ]
+        for i, numbering in enumerate(numberings)
+    ]
+    numberings = [
+        all([
+            wi.is_complete(wi.subgraph(graphs[i], list(nodes)))
+            for nodes in numbering
+        ])
+        for i, numbering in enumerate(numberings)
+    ]
+    assert(all(numberings))
