a16571bfa2e704038227010e0e93fc45a8fa3ddb
AlessioZanga <alessio.zanga@outlook.it>
2020-04-01 23:47:37 +0200

    Add subgraphs

--- weighted_imputation/__init__.py
+++ weighted_imputation/__init__.py
@@ -2,7 +2,8 @@ from . import data, generators
 from .algorithms import (BFS, DFS, MCS, ancestors, boundary, chain_of_cliques,
                          children, descendants, family, is_complete,
                          junction_tree, maximal_cliques, moralize, neighbors,
-                         numbering, parents, perfect_numbering, triangulate)
+                         numbering, parents, perfect_numbering, subgraph,
+                         triangulate)
 from .backends import disable_alternative_backends, force_alternative_backends
 from .structures import (BayesianNetwork, ConditionalProbabilityTable,
                          DirectedGraph, Graph, JointProbabilityTable,
