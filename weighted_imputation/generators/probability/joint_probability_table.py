3c9885e20aceeeee1731911d04969e767466c68e
AlessioZanga <alessio.zanga@outlook.it>
2020-04-18 17:41:36 +0200

    Remove useless test and generators

--- weighted_imputation/generators/probability/joint_probability_table.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def generate_joint_probability_table(N):
-    numBins = 3
-    data = np.random.randn(1000, N)
-    jpt, edges = np.histogramdd(data, bins=numBins)
-    jpt /= jpt.sum()
-    return jpt
