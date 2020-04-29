2c11de0832d5678b019f08d01a0bb70c5a6dfefd
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2020-04-29 16:08:59 +0200

    Add absolute frequencies for sublist of columns

--- weighted_imputation/structures/datasets.py
+++ weighted_imputation/structures/datasets.py
@@ -9,6 +9,9 @@ class Dataset:
     def absolute_frequencies(self):
         return self.data.groupby(list(self.data.columns)).size().reset_index(name='count')
 
+    def absolute_frequencies_sublist(self, columns: list):
+        return self.data.groupby(columns).size().reset_index(name='count')
+
     @classmethod
     def from_file(cls, path: str):
         dataset = pd.read_csv(
