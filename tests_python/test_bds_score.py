0420faf36c481b91e823570d28d348889efb2639
AlessioZanga <alessio.zanga@outlook.it>
2020-05-21 10:25:45 +0200

    Update BDS score tests

--- tests_python/test_bds_score.py
+++ tests_python/test_bds_score.py
@@ -1,6 +1,7 @@
 import numpy as np
 import rpy2.robjects as robjects
-from math import isclose
+from os.path import abspath, dirname, join
+from math import log, isclose
 from . import weighted_imputation as wi
 
 def test_bds_score():
@@ -24,3 +25,24 @@ def test_bds_score():
         bds_r = dict(zip(bds_r.names, list(bds_r)))
         for k, v in bds_python_nodes.items():
             assert(isclose(v, bds_r[k]))
+
+
+def test_bds_score_suzuki_2016():
+    refs = [
+        '../test_data/suzuki_2016_1.csv',
+        '../test_data/suzuki_2016_2.csv'
+    ]
+    refs = [abspath(join(dirname(__file__), ref)) for ref in refs]
+    refs = [wi.Dataset.from_csv(ref) for ref in refs]
+    dags = ['[X|Z:W][Z][W][Y]', '[X|Z:W:Y][Z][W][Y]']
+    outs = [log(0.03262539), log(3.90625e-07)]
+
+    for i, ref in enumerate(refs):
+        for dag in dags:
+            _, nodes = wi.bds_score(dag, ref, with_nodes=True)
+            assert(isclose(
+                nodes['X'],
+                outs[i],
+                rel_tol=1e-8,
+                abs_tol=1e-8
+            ))
