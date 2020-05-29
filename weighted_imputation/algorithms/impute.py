1d53c9944a3537c942c153046abfb82d7481c601
AlessioZanga <alessio.zanga@outlook.it>
2020-05-29 19:29:27 +0200

    Revert "Update impute to multithread version"
    This reverts commit 1c9628df67ec00e1d94de48ce2a1cbcccfb8b86b.

--- weighted_imputation/algorithms/impute.py
+++ weighted_imputation/algorithms/impute.py
@@ -1,57 +1,43 @@
 from __future__ import annotations
-
-import numpy as np
-import pandas as pd
-
-from ..structures import Dataset
 from .junction_tree import junction_tree
-
-from multiprocessing import Pool, cpu_count
+from ..structures import Dataset
+from copy import deepcopy
+import pandas as pd
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import Dict, List
-    from ..structures import BayesianNetwork, JunctionTree
+    from ..structures import BayesianNetwork
 
 
 def impute(network: BayesianNetwork, dataset: Dataset) -> Dataset:
     jt = junction_tree(network)
-    cols = dataset.columns()
-    data = dataset.data.copy()
+    data = deepcopy(dataset.data)
     nans = data[data.isnull().any(axis=1)]
-    nans = nans.astype(str, copy=False).groupby(cols).indices
-
-    nans = [
-        (jt, dict(zip(cols, values)), indices)
-        for values, indices in nans.items()
-    ]
-
-    pool = Pool(cpu_count())
-    nans = pool.starmap(_impute_nans, nans)
-    pool.close()
-    pool.join()
-
-    for (query, indices) in nans:
-        data.loc[indices, query.keys()] = list(query.values())
+    cache = {}
+    for index, row in nans.iterrows():
+        query = row.to_dict()
+        evidence = {
+            k: v
+            for k, v in query.items()
+            if not pd.isnull(v)
+        }
+        key = str(evidence)
+        if key not in cache.keys():
+            query = sorted(query.keys() - evidence.keys())
+            jte = jt.set_evidence(**evidence)
+            query = jte.query('joint', query)[0]
+            query = [
+                query.marginalize([variable])
+                for variable in query.variables()
+            ]
+            query = {
+                q.dims[0]: q.coords[q.dims[0]].values[q.argmax()]
+                for q in query
+            }
+            cache[key] = query
+        query = cache[key]
+        for key, value in query.items():
+            data.loc[[index], [key]] = value
     return Dataset(data)
-
-
-def _impute_nans(jt: JunctionTree, query: Dict, indices: List):
-    evidence = {
-        k: v
-        for k, v in query.items()
-        if v != str(np.nan)
-    }
-    query = list(query.keys() - evidence.keys())
-    jte = jt.set_evidence(**evidence)
-    query = jte.query('joint', query)[0]
-    query = [
-        query.marginalize([variable])
-        for variable in query.variables()
-    ]
-    query = {
-        q.dims[0]: q.coords[q.dims[0]].values[q.argmax()]
-        for q in query
-    }
-    return (query, indices)
