5b3e699ad6dbbfb9156e84ba5c2b6343de45dfee
AlessioZanga <alessio.zanga@outlook.it>
2020-06-08 18:11:07 +0200

    Add specific repo settings in R install

--- .travis/unix_r_install.sh
+++ .travis/unix_r_install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo R -e 'install.packages("BiocManager")'
+sudo R -e 'install.packages("BiocManager", repos = "https://cloud.r-project.org/")'
 sudo R -e 'BiocManager::install(c("bnlearn", "gRain"))'
