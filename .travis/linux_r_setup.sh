366daa4399be56b5bcffa7c58671b2d10f01c063
AlessioZanga <alessio.zanga@outlook.it>
2020-06-18 11:28:04 +0200

    Changed R version to 3.6

--- .travis/linux_r_setup.sh
+++ .travis/linux_r_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 51716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran36/'
 sudo apt-get update
 sudo apt-get -y install r-base r-base-dev
