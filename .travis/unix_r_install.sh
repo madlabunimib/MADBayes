cc8bd6923d30ab1311685a861e2dae1c2fc1210f
AlessioZanga <alessio.zanga@outlook.it>
2020-06-18 11:36:50 +0200

    Fix gRain install

--- .travis/unix_r_install.sh
+++ .travis/unix_r_install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo R -e 'install.packages("BiocManager", repos = "https://cloud.r-project.org/")'
-sudo R -e 'BiocManager::install(c("bnlearn", "gRain"))'
+sudo R -e 'BiocManager::install(c("bnlearn", "graph", "RBGL", "Rgraphviz"))'
+sudo R -e 'install.packages("gRain", dependencies = TRUE, repos = "https://cloud.r-project.org/")'
--- .travis/win32_r_install.bat
+++ .travis/win32_r_install.bat
@@ -1,3 +1,4 @@
 @ECHO ON
-R.exe -e 'install.packages("BiocManager")'
-R.exe -e 'BiocManager::install(c("bnlearn", "gRain"))'
+R.exe -e 'install.packages("BiocManager", repos = "https://cloud.r-project.org/")'
+R.exe -e 'BiocManager::install(c("bnlearn", "graph", "RBGL", "Rgraphviz"))'
+R.exe -e 'install.packages("gRain", dependencies = TRUE, repos = "https://cloud.r-project.org/")'
