928fb2d9ba4a73bc7198af275cf8e6a1aa6d1725
AlessioZanga <alessio.zanga@outlook.it>
2020-07-01 18:20:17 +0200

    Update R configuration

--- .travis/unix_r_install.sh
+++ .travis/unix_r_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo R -e 'install.packages("BiocManager", repos = "https://cloud.r-project.org/")'
-sudo R -e 'BiocManager::install(c("bnlearn", "graph", "RBGL", "Rgraphviz"))'
+sudo R -e 'install.packages("BiocManager", version = "3.10", repos = "https://cloud.r-project.org/")'
+sudo R -e 'BiocManager::install(version = "3.10", c("bnlearn", "graph", "RBGL", "Rgraphviz"))'
 sudo R -e 'install.packages("gRain", dependencies = TRUE, repos = "https://cloud.r-project.org/")'
