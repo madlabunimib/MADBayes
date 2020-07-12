aa0421e02acca58760f511bd9b16e11ef6c06045
AlessioZanga <alessio.zanga@outlook.it>
2020-07-12 11:29:30 +0200

    Update project structure

--- scripts/compute_query.R
+++
@@ -1,8 +0,0 @@
-library(bnlearn, gRain)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-BASE_PATH = "~/madlab_madbayes/madbayes/networks"
-file = "asia.bif"
-fitted = read.bif(paste(BASE_PATH, file, sep="/"))
-fitted = mutilated(fitted, evidence = list('lung' = 'yes'))
-probs = gRain::querygrain(as.grain(fitted), nodes = c('asia', 'xray', 'tub'), type = 'joint', result='data.frame')
