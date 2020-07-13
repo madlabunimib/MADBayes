43f8ed370aa09b7ca15d232cfe2e79dca0c36098
AlessioZanga <alessio.zanga@outlook.it>
2020-07-13 21:13:06 +0200

    Add chain of cliques test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
+import warnings; warnings.filterwarnings('ignore', category=DeprecationWarning)
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
--- tests/test_chain_of_cliques.py
+++ tests/test_chain_of_cliques.py
@@ -2,35 +2,23 @@ import networkx as nx
 from functools import reduce
 from . import madbayes as mb
 
-def test_perfect_numbering():
-    # Generate random graphs from nodes count
-    nodes = [2, 10, 25, 50]
-    graphs = [
-        nx.gnp_random_graph(n, 0.5, directed=True)
-        for n in nodes
-    ]
-    # Transform to Wi Graphs
-    graphs = [mb.Graph.from_networkx(G) for G in graphs]
-    graphs = [mb.triangulate(graph) for graph in graphs]
-    chains = [mb.chain_of_cliques(graph) for graph in graphs]
-    chains = [[set(clique) for clique in chain] for chain in chains]
-    running = [
-        [
+
+def test_chain_of_cliques():
+    for nodes in [10, 25, 50, 100]:
+        g = mb.backend.Graph.random(nodes, 0.5)
+        g = mb.backend.chordal(g)
+        cliques = mb.backend.maximal_cliques(g)
+        alpha = mb.backend.maximum_cardinality_search(g)
+        chain = mb.backend.chain_of_cliques(cliques, alpha)
+        chain = [set(clique) for clique in chain]
+        running = [
             chain[i].intersection(
-                reduce(lambda a,b: a.union(b), chain[:i], set())
+                reduce(lambda a, b: a.union(b), chain[:i], set())
             )
             for i, _ in enumerate(chain)
         ]
-        for chain in chains
-    ]
-    running = [
-        all([
-            any([
-                run.issubset(set(clique))
-                for clique in chains[i]
-            ])
-            for run in runs
+        running = any([
+            run.issubset(set(run))
+            for run in running
         ])
-        for i, runs in enumerate(running)
-    ]
-    assert(all(running))
+        assert(running)
